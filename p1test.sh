05f920389e45e089e26d392d412e4e43bf1443ec
Lele ZHAO <lzha9942@uni.sydney.edu.au>
2024-05-19 08:39:35 +1000

    1

--- p1test.sh
+++ p1test.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-set -e
-
-include_path = include
-src_path = src
-resources_path = resources
-pkgs_path = $resources_path/pkgs
-getting_started_path = $resources_path/getting_started
-
-pkgchecker = pkgchecker
-
 echo "Running p1 tests..."
 
-file1_bpkg = $pkgs_path/file1.bpkg
-file4_bpkg = $pkgs_path/file4.bpkg
-
-./pkgchecker > file1.out
-./pkgchecker > file4.out
+./pkgchecker resources/pkgs/file1.bpkg -all_hashes > file1.out
+./pkgchecker resources/pkgs/file4_bpkg -all_hashes > file4.out
 
 diff -q file1.out ./tests/p1test/file1.expected
 diff -q file4.out ./tests/p1test/file4.expected
+
+
